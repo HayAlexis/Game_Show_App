1f8a7577e62ae6c72b7132db9e48167a98fc972d
HayAlexis <79810812+HayAlexis@users.noreply.github.com>
2022-02-19 17:02:00 -0500

    Array Phrases

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const keyBoard = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');  
 const start = document.querySelector('.btn__reset'); 
 
+let missed = 0;
+
+//Start Game Button
+start.addEventListener('click', e => {
+    overlay.style.display = "none";
+});
+
 //Array Phrases
 const phrases = [
     'I solemnly swear that I am up to no good',
@@ -11,13 +18,10 @@ const phrases = [
     'The ones that love us never really leave us',
 ]
 
-let missed = 0;
-
-
-//Start Game Button
-start.addEventListener('click', e => {
-    overlay.style.display = "none";
-});
-
-
-
+//Random Phrase Array Function
+function getRandomPhrasesAsArray(phrases) {
+    const phraseIndex = Math.floor(Math.random()*phrases.length);
+    const phraseSelect = phrases[phraseIndex].toUpperCase();
+    const phraseSplit = phraseSelect.split("");
+    return phraseSplit;
+}
