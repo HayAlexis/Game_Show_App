d9d46b004c83bc570479d9bf6bf6bbd845040b56
HayAlexis <79810812+HayAlexis@users.noreply.github.com>
2022-02-19 16:14:40 -0500

    start btn not working

--- js/app.js
+++ js/app.js
@@ -1,5 +1,12 @@
-const qwerty = document.getElementById(qwerty);
-const phrase = document.getElementById(phrase);
+const keyBoard = document.getElementById('qwerty'); 
+const phrase = document.getElementById('phrase');  
+const start = document.querySelector('.btn__reset'); 
+
+
 let missed = 0;
 
 
+//Start Game Button
+start.addEventListener('click', e => {
+    overlay.style.display = "none";
+});
