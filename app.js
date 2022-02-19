9187acfcf32e9cb093474d8591e6abf3951aa40c
HayAlexis <79810812+HayAlexis@users.noreply.github.com>
2022-02-19 16:53:32 -0500

    create harry potter phrases

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ const keyBoard = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');  
 const start = document.querySelector('.btn__reset'); 
 
+//Array Phrases
+const phrases = [
+    'I solemnly swear that I am up to no good',
+    'I love magic',
+    'Yer a wizard Harry',
+    'It does not do to dwell on dreams and forget to live',
+    'The ones that love us never really leave us',
+]
 
 let missed = 0;
 
@@ -9,5 +17,7 @@ let missed = 0;
 //Start Game Button
 start.addEventListener('click', e => {
     overlay.style.display = "none";
-
 });
+
+
+
