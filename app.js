7bef4301b68db331e2c546aed9c71e1ac996b2a0
HayAlexis <79810812+HayAlexis@users.noreply.github.com>
2022-02-19 17:36:53 -0500

    Grey Boxes appear

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const keyBoard = document.getElementById('qwerty'); 
 const phrase = document.getElementById('phrase');  
 const start = document.querySelector('.btn__reset'); 
+const ul = document.querySelector('#phrase ul');
+const overlay = document.getElementById('overlay');
 
 let missed = 0;
 
@@ -24,4 +26,38 @@ function getRandomPhrasesAsArray(phrases) {
     const phraseSelect = phrases[phraseIndex].toUpperCase();
     const phraseSplit = phraseSelect.split("");
     return phraseSplit;
+}
+
+const selected = getRandomPhrasesAsArray(phrases);
+
+//Selected Letters to Display
+function addPhraseToDisplay (phrase) {
+    for(let i = 0; i < phrase.length; i++) {
+        const list = document.createElement('li');
+        list.textContent = phrase[i];
+        ul.appendChild(list);
+
+        if (phrase[i] === ' ') {
+            list.className = 'space';
+        } else {
+            list.className = 'letter'
+        }
+    }
+}
+
+const selectedDisplay = addPhraseToDisplay(selected);
+
+//Check if letter is in phrase
+function checkLetter(button){
+    const letterList = document.querySelectorAll('.letter');
+    let matchLetter = null;
+
+    for ( let i = 0; i < letterList.length; i++) {
+        if (button.textContent === letterList[i].textContent) {
+            letterList[i].classList.add('show');
+            letterList[i].style.transition = '0.5s ease-in-out';
+            matchLetter = true;
+        }
+    }
+    return matchLetter;
 }
