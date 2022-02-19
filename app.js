1c74d4a611bc2916c9d2d12646e2ded46477e015
HayAlexis <79810812+HayAlexis@users.noreply.github.com>
2022-02-19 16:39:40 -0500

    Fixed start button
    needed to update file path.

---
+++ app.js
@@ -0,0 +1,13 @@
+const keyBoard = document.getElementById('qwerty'); 
+const phrase = document.getElementById('phrase');  
+const start = document.querySelector('.btn__reset'); 
+
+
+let missed = 0;
+
+
+//Start Game Button
+start.addEventListener('click', e => {
+    overlay.style.display = "none";
+
+});
